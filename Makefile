063416b8336e0fec529aea0e870d0e2080930dce
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-03-19 16:59:20 -0700

    Tooling for theme development + live reloading (#116)
    * Add basic theme with page content and TOCs
    
    * Setup .nvmrc and .gitignore for node.js dev
    
    * Add gulpfile.js and dev-server.js
    
    * Setup make targets for theme dev
    
    * Add linters for JS and SCSS + commit hooks
    
    * Update deploy workflow
    
    * Update README.md
    
    * Change Napari -> napari in README and comments
    
    * Clean up makefile and move `make dev` to package.json script
    
    * Minor text changes
    
    * Replace Makefile with npm scripts
    
    * Update README.md

--- Makefile
+++
@@ -1,5 +0,0 @@
-.PHONY: clean
-
-clean:
-	rm -rf _build/html
-	find api/stable/ ! -name 'index.rst' -type f -exec rm -f {} +
---
+++ dev-server.js
@@ -0,0 +1,95 @@
+/**
+ * Script for re-running `jupyter-book build` on script changes.
+ *
+ * TODO There's probably a smarter way to serve the re-built SCSS and JS assets
+ * using some proxying magic. Instead of rebuilding the entire docs for SCSS
+ * and JS changes, we could probably serve the re-compiled assets directly from
+ * `theme/napari/static/dist` instead of rebuilding and using the updated
+ * assets in `_build/html/_static/dist`.
+ */
+
+const chokidar = require('chokidar');
+const { spawn } = require('child_process');
+
+const express = require('express');
+const logger = require('morgan');
+const reload = require('reload');
+
+const PUBLIC_DIR = '_build/html';
+const DEFAULT_PORT = 8080;
+const IGNORED_FILES = ['.github', '_build', 'node_modules'];
+
+class DevServer {
+  app = null;
+  buildProcess = null;
+  reloader = null;
+
+  reloadBrowser() {
+    console.log('Reloading the browser!');
+    this.reloader.reload();
+  }
+
+  /**
+   * Runs `jupyter-book` to build the napari docs as a subprocess. Running
+   * processes are cancelled to ensure the build is up-to-date.
+   */
+  buildDocs() {
+    // Kill existing build if there is one
+    if (this.buildProcess) {
+      console.log('Cancelling existing build');
+      this.buildProcess.kill();
+    }
+
+    this.buildProcess = spawn('jupyter-book', ['build', '.'], {
+      // Pipe process output to terminal
+      stdio: 'inherit',
+    });
+    this.buildProcess.once('exit', (code) => {
+      this.buildProcess = null;
+
+      // Reload on successful run
+      if (this.reloader && code === 0) {
+        this.reloadBrowser();
+      }
+    });
+  }
+
+  /**
+   * Re-builds the napari docs on file changes.
+   */
+  watchDocs() {
+    const watcher = chokidar.watch('.', { ignored: IGNORED_FILES });
+
+    watcher.on('change', (path) => {
+      console.log(`${path} changed, re-building docs`);
+      this.buildDocs();
+    });
+
+    // Build docs on initial run
+    this.buildDocs();
+  }
+
+  /**
+   * Starts the dev server using express and reload.
+   */
+  async startDevServer() {
+    this.app = express();
+    this.app.use(logger('dev'));
+    this.app.use(express.static(PUBLIC_DIR));
+
+    this.reloader = await reload(this.app);
+    this.app.listen(DEFAULT_PORT, () =>
+      console.log(
+        `Started napari.org dev server at http://localhost:${DEFAULT_PORT}`,
+      ),
+    );
+  }
+
+  async start() {
+    await this.startDevServer();
+    this.watchDocs();
+  }
+}
+
+const server = new DevServer();
+server.start();

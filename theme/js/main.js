7a3595a57bbe6a8e22c32c882be056a157c356e7
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-04-06 11:51:13 -0700

    App Bar and Search components (#122)
    * Assets for app bar
    
    * Mixin for layout breakpoints
    
    * CSS vars for container dimensions
    
    * Implement app bar component
    
    * Fix SCSS variable typo
    
    * Minor SCSS changes for consistency
    
    * Make font responsive to viewport
    
    * Swap napari.png for napari.svg
    
    * Convert sticky header to position: fixed for better compat
    
    * Fix scaling for app bar, margins, and image width
    
    * Adjust clamp values

--- theme/js/main.js
+++ theme/js/main.js
@@ -1,5 +1,13 @@
+function handleMenuClick(e) {
+  // eslint-disable-next-line no-console
+  console.log('Clicked on menu button:', e);
+}
+
 function main() {
   document.body.parentNode.classList.remove('no-js');
+
+  const menuButton = document.querySelector('.app-bar__menu-button');
+  menuButton.addEventListener('click', handleMenuClick);
 }
 
 document.addEventListener('DOMContentLoaded', main);

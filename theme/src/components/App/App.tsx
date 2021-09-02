a28bda91338704d41aa991de476f8938f0832234
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-09-02 10:21:53 -0700

    Added in-page table of contents

--- theme/src/components/App/App.tsx
+++ theme/src/components/App/App.tsx
@@ -5,28 +5,55 @@ import '@/scss/main.scss';
 import clsx from 'clsx';
 
 import { AppBar } from '@/components/AppBar';
-import { MediaContextProvider } from '@/components/media';
+import { Media, MediaContextProvider } from '@/components/media';
+import { TableOfContents } from '@/components/TableOfContents';
 import { useJupyterBookData } from '@/context/jupyterBook';
 
 import styles from './App.module.scss';
+
+function InPageTableOfContents() {
+  const { pageHeaders } = useJupyterBookData();
+  return <TableOfContents headers={pageHeaders} />;
+}
 
 function Content() {
   const { pageBodyHtml } = useJupyterBookData();
 
   return (
-    <div className="grid grid-cols-napari-5 justify-center gap-6 screen-495:gap-12">
-      <div />
+    <div
+      className={clsx(
+        'grid grid-cols-3',
+        'screen-875:grid-cols-4 screen-1425:grid-cols-napari-5',
+
+        'justify-center gap-6 screen-495:gap-12',
+        'p-6 screen-495:p-12',
+      )}
+    >
+      <Media greaterThanOrEqual="screen-1150">
+        <p>Global TOC placeholder</p>
+      </Media>
 
       <div
         className={clsx(
-          'col-start-2 col-span-3 prose max-w-full',
-          styles.content,
+          'col-span-3 screen-1425:col-start-2 screen-1425:col-span-3',
         )}
-        // eslint-disable-next-line react/no-danger
-        dangerouslySetInnerHTML={{ __html: pageBodyHtml }}
-      />
+      >
+        <Media lessThan="screen-1425">
+          <div className="my-6">
+            <InPageTableOfContents />
+          </div>
+        </Media>
 
-      <div />
+        <div
+          className={clsx('prose max-w-full', styles.content)}
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{ __html: pageBodyHtml }}
+        />
+      </div>
+
+      <Media greaterThanOrEqual="screen-1425">
+        <InPageTableOfContents />
+      </Media>
     </div>
   );
 }

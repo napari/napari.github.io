195633eb0173d918766366a038425502cc609ac9
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-09-08 13:50:20 -0700

    Render links in mobile global TOC

--- theme/src/components/AppBar.tsx
+++ theme/src/components/AppBar.tsx
@@ -1,23 +1,58 @@
 import clsx from 'clsx';
 import { useState } from 'react';
 
-import { Menu, NapariLogo } from '@/components/icons';
+import { ExternalLink, Menu, NapariLogo } from '@/components/icons';
 import { Link } from '@/components/Link';
 import { Media } from '@/components/media';
 import { MenuDrawer } from '@/components/MenuDrawer';
 import { SearchInput } from '@/components/SearchInput';
+import { useJupyterBookData } from '@/context/jupyterBook';
+import { LinkInfo } from '@/types';
+
+/**
+ * Checks if the string is an external link. This works by using the value to
+ * create a URL object. URL objects will throw errors for relative URLs if a
+ * base URL isn't provided, so an error will indicate that the URL is an absolute URL.
+ *
+ * One limitation with this is it only checks for absolute URLs since Jupyter
+ * Book uses relative URLs for the TOC. If an absolute URL is used that has the
+ * same host, it'll be treated as an external URL, but so far there are no use cases.
+ *
+ * @param url The string to check.
+ * @returns True if the string is an external URL, false if relative.
+ */
+function isExternalLink(url: string) {
+  try {
+    return url !== '#' && !!new URL(url);
+  } catch (_) {
+    return false;
+  }
+}
 
 /**
  * App bar component that renders the home link, search bar, and menu.
  */
 export function AppBar() {
   const [visible, setVisible] = useState(false);
+  const { globalHeaders, rootGlobalHeaders } = useJupyterBookData();
+
+  const links: LinkInfo[] = rootGlobalHeaders.map((header) => {
+    const { href, text } = globalHeaders[header];
+    const isExternal = isExternalLink(href);
+
+    return {
+      link: href,
+      title: text,
+      icon: isExternal && <ExternalLink />,
+      newTab: isExternal,
+    };
+  });
 
   return (
     <>
       <Media lessThan="screen-1150">
         <MenuDrawer
-          items={[]}
+          items={links}
           onClose={() => setVisible(false)}
           onOpen={() => setVisible(true)}
           visible={visible}

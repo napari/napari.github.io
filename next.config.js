bb46e36578c53bff8c659e38e0dc65e15168c979
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-10-19 10:34:11 -0700

    Fix plausible integration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
+const { EnvironmentPlugin } = require('webpack');
+
 module.exports = {
   webpack(config) {
     // Sets BABEL_ENV to `client` or `server` depending on the Next.js build.
     // This is required for the Material UI + babel import plugin to work.
     process.env.BABEL_ENV = config.name;
+
+    config.plugins.push(
+      new EnvironmentPlugin({
+        // Environment variable for enabling plausible analytics. By default,
+        // analytics are enabled for production and staging deployments, but can
+        // be enabled manually for testing.
+        PLAUSIBLE: 'false',
+      }),
+    );
 
     return config;
   },

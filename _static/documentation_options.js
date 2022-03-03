7e79b5e731083b932487dff1505d7b61c25e3718
jni <jni@users.noreply.github.com>
2022-03-03 11:47:10 +0000

    deploy: napari/napari@c2058599bee01cdbe0bc247ec4f72ef93929b2a5

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+ge1ebbc2',
+    VERSION: '0.4.15rc1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

47f7cd46aed3e0a16f1ce8dbba095fc5719e2156
potating-potato <potating-potato@users.noreply.github.com>
2022-03-18 23:38:28 +0000

    deploy: napari/napari@a3705f946ffaa052188269223b6991d65ef71d58

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+ga8e5023',
+    VERSION: '0.1.dev1+ga3705f9',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

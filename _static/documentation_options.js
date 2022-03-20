2e346a7c0c5f94dbbda6137fe8e06d4a86cd756b
tlambert03 <tlambert03@users.noreply.github.com>
2022-03-20 04:35:12 +0000

    deploy: napari/napari@71f6b11d621e81d1e1e0348932c4e15d8c76f679

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+ga3705f9',
+    VERSION: '0.1.dev1+g71f6b11',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

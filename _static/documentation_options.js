5f69d70a8a1de9d4c220ed70fa7773d5e0ee4c57
sofroniewn <sofroniewn@users.noreply.github.com>
2022-03-26 04:43:19 +0000

    deploy: napari/napari@427ac948ef9dcb1890ae3d69024a51c756f16747

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g1d84757',
+    VERSION: '0.1.dev1+g427ac94',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

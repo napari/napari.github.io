c8e54800fd48bff2875b633c374f372d3e8e7276
alisterburt <alisterburt@users.noreply.github.com>
2022-04-25 11:43:33 +0000

    deploy: napari/napari@4121f2efc5a3130343067df431ab17c5c399cb8c

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g68d2c1b',
+    VERSION: '0.1.dev1+g4121f2e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

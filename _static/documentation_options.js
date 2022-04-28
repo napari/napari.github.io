7c30f6e9920289dda38b1908a36cd9206c038f08
Carreau <Carreau@users.noreply.github.com>
2022-04-28 11:54:23 +0000

    deploy: napari/napari@413488578b4ca22e09c300e194978c1ce6bf6f06

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g903f3e0',
+    VERSION: '0.1.dev1+g4134885',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

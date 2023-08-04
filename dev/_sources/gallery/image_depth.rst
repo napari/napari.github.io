509c9434326abe62c18d691ed9bca0e0351bf4bb
jni <jni@users.noreply.github.com>
2023-08-04 00:39:10 +0000

    deploy: napari/docs@3d97ad294e3c32ec7d60faacb69d867fce834a0b

--- dev/_sources/gallery/image_depth.rst
+++ dev/_sources/gallery/image_depth.rst
@@ -63,7 +63,7 @@ Image depth
 
 .. rst-class:: sphx-glr-timing
 
-   **Total running time of the script:** ( 0 minutes  14.262 seconds)
+   **Total running time of the script:** ( 0 minutes  14.283 seconds)
 
 
 .. _sphx_glr_download_gallery_image_depth.py:
--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.0a2.dev264+gcde0cf3b',
+    VERSION: '0.5.0a2.dev265+g91868815',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
